7cb02f39ef5afb89b0a6ce7a983f16821108be6a
SantatraM <santatryn@gmail.com>
2023-12-15 14:22:15 +0300

    script_sql

--- build/web/view.sql
+++
@@ -1,5 +0,0 @@
-create view v_bouquetActivite as 
-    select ba.*,a.nomActivite,b.nomBouquet
-    from bouquetActivite ba
-    join activite a on a.idActivite = ba.idActivite
-    join bouquet b on b.idBouquet = ba.idBouquet;
